a364f64ea955cd1a0b12815d7346ab534b686c60
virendra <virendrachouhan1494@gmail.com>
2022-07-30 20:31:33 +0530

    Merge

---
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Line_Comparison_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison .");
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter values x1 and y1");
+            //using type casting to convert to int
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter values x2 and y2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of the Line is : " + length);
+        }
+    }
+}
